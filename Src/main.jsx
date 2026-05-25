1419e0e56ff38d90541b90887f88477fbca05c3d
Tran Duc Manh <tranducmanh.qct@gmail.com>
2026-05-25 23:28:01 +0700

    Update main.jsx

--- Src/main.jsx
+++ Src/main.jsx
@@ -1,6 +1,8 @@
+import React, { useEffect, useMemo, useState } from "react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
